9b79f75132818efea764523a8874a4c145db5dde
killme <bram.wall@gmail.com>
2012-05-12 08:36:57 +0200

    cleanup

--- protected/runtime/gii-1.1.10/CrudCode.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'template' => 'admin',
-  'baseControllerClass' => 'Controller',
-);
--- protected/runtime/gii-1.1.10/FormCode.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'template' => 'default',
-  'viewPath' => 'as.views.page',
-);
